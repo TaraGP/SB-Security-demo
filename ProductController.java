e140620d6b6276f24cbfdc1cc51237123f070fb8
TaraGP <115979347+TaraGP@users.noreply.github.com>
2023-05-03 11:10:20 +0530

    Add files via upload

---
+++ ProductController.java
@@ -0,0 +1,40 @@
+package com.JSecurityDemo.demo.controller;
+
+import com.JSecurityDemo.demo.dto.Product;
+import com.JSecurityDemo.demo.entity.UserInfo;
+import com.JSecurityDemo.demo.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome this endpoint is not secure";
+    }
+
+    @PostMapping("/new")
+    public String addNewUser(@RequestBody UserInfo userInfo){
+        return service.addUser(userInfo);
+    }
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public List<Product> getAllTheProducts() {
+        return service.getProducts();
+    }
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    public Product getProductById(@PathVariable int id) {
+        return service.getProduct(id);
+    }
+}
